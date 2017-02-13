5762caf63f2b462528817a9be886c574bc49d6b8
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-02-13 17:40:49 -0500

    deal with selected options depending on current page

--- js/app.js
+++ js/app.js
@@ -7,6 +7,11 @@ $("#menu a").each(function(){
     var $anchor = $(this);
     //Create an option
     var $option = $("<option></option>");
+
+    //Deal with selected options depending on current page
+    if ($anchor.parent().hasClass("selected")) {
+        $option.prop("selected", true);
+    };
     //Option's value is the href
     $option.val($anchor.attr("href"));
     //Option's text is the text of links
